e37e7d9b9e109ca3fd9fceeb3779607b0c669c8f
keleilei <keleilei@gmail.com>
2016-10-11 23:45:43 +0800

    add background starry sky

--- src/main/webapp/js/particles.random.js
+++ src/main/webapp/js/particles.random.js
@@ -1,5 +1,5 @@
 /**
  * Created by kelei on 2016/10/9.
  */
-var pbs = ['js/assets/particles_default.json','js/assets/particles_snow.json'];
+var pbs = ['js/assets/particles_default.json', 'js/assets/particles_snow.json', 'js/assets/particles_starrysky.json'];
 particlesJS.load('particles-js', pbs[parseInt(Math.random()*pbs.length)]);
